89d355dbbb412080575ca4f7656913cdc43c44de
Mason <nosam1998@users.noreply.github.com>
2020-08-02 04:31:04 -0700

    Added some style

--- URLShortenerDjango/settings.py
+++ URLShortenerDjango/settings.py
@@ -113,3 +113,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'URLShortenerDjango/staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
