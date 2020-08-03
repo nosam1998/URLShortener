813b8b583528f42054a27b2e2eacd467f4d77f02
Mason <nosam1998@users.noreply.github.com>
2020-08-03 06:31:51 -0700

    Turned debug mode off

--- URLShortenerDjango/settings.py
+++ URLShortenerDjango/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '+6+yiwzdwc94n@yvdk4$1$*xf^&4m(1sb*c+fbf*!zsbcljw(('
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'https://nosam1998-url-shortener.herokuapp.com/', '*']
 
