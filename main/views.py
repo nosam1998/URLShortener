61900aaedba1bb62d4df796ae80e385b9825e11e
Mason <nosam1998@users.noreply.github.com>
2020-08-03 06:28:27 -0700

    Added unique slugs and auto copy to clipboard

--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
+import random
+import re
+import string
 from django.shortcuts import render, redirect
+from django.utils.text import slugify
 from django.views import View
 
 from .forms import CreateShortUrlForm
@@ -10,7 +14,7 @@ class Home(View):
     form_class = CreateShortUrlForm
 
     def get(self, request):
-        return render(request, self.template_name)
+        return render(request, self.template_name, context={"final_url": "", "no_url": True})
 
     def post(self, request):
         form = self.form_class(request.POST)
@@ -19,13 +23,21 @@ class Home(View):
             slug = form.cleaned_data.get('slug')
             # TODO Validate that the slug doesn't contain any bad characters
 
-            if ShortenedUrl.objects.filter(slug=slug).count() == 0:
-                new_short_url = ShortenedUrl()
-                new_short_url.slug = slug
-                new_short_url.original_url = form.cleaned_data.get('original_url')
+            new_slug = slugify(slug)
+            slug_search = ShortenedUrl.objects.filter(slug=new_slug)
 
-                new_short_url.save()
-        return redirect("/")
+            if slug_search.count() != 0:
+                db_row_count = ShortenedUrl.objects.all().count() + 1
+                new_slug = "{s}-{num}".format(s=new_slug, num=db_row_count)
+
+            new_short_url = ShortenedUrl()
+            new_short_url.slug = new_slug
+            new_short_url.original_url = form.cleaned_data.get('original_url')
+            new_short_url.save()
+
+            return render(request, self.template_name, context={"final_url": new_slug, "no_url": False})
+        else:
+            return redirect("/")
 
 
 class RedirectUser(View):
@@ -37,4 +49,4 @@ class RedirectUser(View):
         elif obj.count() == 0:
             return redirect("/")
         else:
-            return redirect("Something Went wrong")
+            return redirect("/")
